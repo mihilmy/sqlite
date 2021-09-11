892449db24d9e6899984494647683f790d2143fa
Omar <mihilmy@yahoo.com>
2021-09-11 13:41:23 -0700

    P2: Simple meta and sql processor vm

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,7 @@ use std::process;
 fn main() {
     loop {
         let cmd = get_command();
-
-        if cmd.trim() == ".exit" {
-            process::exit(0);
-        } else {
-            print!("Unrecognized command {}", cmd);
-        }
+        cmd_processor(&cmd);
     }
 }
 
@@ -21,4 +16,58 @@ fn get_command() -> String {
     io::stdin().read_line(&mut cmd).expect("Failed to read line");
 
     return cmd;
+}
+
+fn cmd_processor(cmd: &str) {
+   if cmd.starts_with('.')  {
+        meta_cmd_processor(cmd);
+    } else {
+        sql_cmd_processor(cmd);
+    }
+}
+
+fn sql_cmd_processor(cmd: &str) {
+    let sql_statement = to_sql_statement(cmd);
+    execute_sql(sql_statement);
+}
+
+fn to_sql_statement(raw_cmd: &str) -> SqlStatement {
+    if raw_cmd.starts_with("insert") {
+        return SqlStatement::Insert;
+    }
+
+    if raw_cmd.starts_with("select") {
+        return SqlStatement::Select;
+    }
+
+    return SqlStatement::Unknown;
+}
+
+fn execute_sql(statement: SqlStatement) {
+    match statement {
+        SqlStatement::Select => {
+            println!("Select from database");
+        },
+        SqlStatement::Insert => {
+            println!("Inserting into database");
+        },
+        SqlStatement::Unknown => {
+            println!("Unrecognized SQL syntax");
+        }
+    }
+}
+
+fn meta_cmd_processor(cmd: &str) {
+    if cmd.trim() == ".exit" || cmd.trim() == "^D" {
+        println!("Bye now ✌️");
+        process::exit(0);
+    } else {
+        print!("Unrecognized command {}", cmd);
+    }
+}
+
+enum SqlStatement {
+    Select,
+    Insert,
+    Unknown,
 }
